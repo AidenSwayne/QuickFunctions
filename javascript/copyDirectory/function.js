ae10ab0bdcd4b3acc8c11b82c0582f48230339f0
AidenSwayne <119467080+AidenSwayne@users.noreply.github.com>
2024-07-14 12:31:48 -0400

    Update function.js

--- javascript/copyDirectory/function.js
+++ javascript/copyDirectory/function.js
@@ -1,15 +1,23 @@
 function copyDirectory(sourceDir, destinationDir) {
-    const fs = require('fs');
-    const path = require('path');
-
     // Delete the destination directory if it already exists
     if (fs.existsSync(destinationDir)) {
         fs.rmSync(destinationDir, { recursive: true, force: true });
     }
 
+    // Create the destination directory
+    fs.mkdirSync(destinationDir, { recursive: true });
+
     // Copy the source directory to the destination directory
-    fs.cpSync(sourceDir, destinationDir, { recursive: true });
+    const items = fs.readdirSync(sourceDir, { withFileTypes: true });
+    for (const item of items) {
+        const srcPath = path.join(sourceDir, item.name);
+        const destPath = path.join(destinationDir, item.name);
+
+        if (item.isDirectory()) {
+            copyDirectory(srcPath, destPath);
+        } else {
+            fs.copyFileSync(srcPath, destPath);
+        }
+    }
     return true;
 }
-module.exports = { copyDirectory };
-module.exports = { copyDirectory };
