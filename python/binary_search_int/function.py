95d2f27bb4451e12b5029371a600dc13aa5b5ad1
AidenSwayne <119467080+AidenSwayne@users.noreply.github.com>
2024-07-15 10:00:41 -0400

    meaningless change

--- python/binary_search_int/function.py
+++ python/binary_search_int/function.py
@@ -5,7 +5,6 @@ def binary_search_int(arr: list[int], target: int) -> int:
     left = 0
     right = len(arr) - 1
     result = -1
-    
 
     while left <= right:
         mid = (left + right) // 2
