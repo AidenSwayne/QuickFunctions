bf2022a7b43d35459f204fec6f5c0ea4fcea88fc
AidenSwayne <119467080+AidenSwayne@users.noreply.github.com>
2024-07-14 22:54:05 -0400

    Update function.cpp
    First part of PR #9

--- c_plus_plus/binary_search_int/function.cpp
+++ c_plus_plus/binary_search_int/function.cpp
@@ -1,23 +1,19 @@
 #include "function.h"
 
 int binary_search_int(const std::vector<int>& arr, int target) {
-    int left = 0;
-    int right = arr.size() - 1;
-    int result = -1;
+    // Use unsigned indices to let compiler skip consideration of negative values.
+    if (arr.empty())
+        return -1;
+    unsigned indices [] = { 0, arr.size() };
     
 
-    while (left <= right) {
-        int mid = (left + right) / 2;
-
-        if (arr[mid] == target) {
-            result = mid;
-            right = mid - 1;
-        } else if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
+    while (indices[0] < indices[1]) {
+        //    Note: Can fail due to overflow, but unlikely to in the test action. Add half of diference to avoid.
+        unsigned mid = (indices[0] + indices[1]) / 2;
+        
+        //    Branches? We don't need no stinkin' branches. (author's note: This is the _other_ type of reference)
+        indices[arr[mid] > target] = mid;
     }
 
-    return result;
+    return (arr[indices[0]] == target) ? static_cast<int>(indices[0]) : -1;
 }
